2cf434da4cfd9601db1e6fac6e87393030db0e4d
singhet <singhet@SCSWL4072>
2024-01-20 12:09:54 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] pets = new string[] {"cat", "dog", "parrot", "guinea pig"};
+        foreach (string pet in pets)
+        {
+            Console.WriteLine(pet);
+            if (pet == "dog")// 
+            {
+                Console.WriteLine("End program because found pet 'dog'.");
+                break;
+            }
+        }
+    }
+}
